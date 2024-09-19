9a12e140ec1c0f58bde3c719ca5770194b9fa281
ChanHeePark0805 <cksgml0805@naver.com>
2024-09-19 17:44:12 +0900

    pch

---
+++ advanced/main.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+
+
+Solar = pd.read_csv('C:/Users/cksgm/Downloads/Data1/Data1/Solar_4.csv')
+Solar["DeliveryDT"]=pd.to_datetime(Solar["DeliveryDT"])
+print('c')
